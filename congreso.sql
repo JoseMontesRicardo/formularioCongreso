f216973aefc8367af022970e1560e9fac8b30413
jose hilario <josehilariomontesricardo@gmail.com>
2015-10-21 13:55:44 -0500

    [start]

---
+++ congreso.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `congreso` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `congreso`;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: congreso
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `registrados`
+--
+
+DROP TABLE IF EXISTS `registrados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registrados` (
+  `Nombres` varchar(50) NOT NULL,
+  `Apellidos` varchar(50) NOT NULL,
+  `Edad` int(3) NOT NULL,
+  `Telefono` varchar(45) NOT NULL,
+  `Correo` varchar(45) NOT NULL,
+  `Comunidad` varchar(45) NOT NULL,
+  `Parroquia` varchar(45) NOT NULL,
+  `Mojuvi` varchar(65) NOT NULL,
+  PRIMARY KEY (`Correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registrados`
+--
+
+LOCK TABLES `registrados` WRITE;
+/*!40000 ALTER TABLE `registrados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registrados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-21 13:49:40
---
+++ server.js
@@ -0,0 +1,87 @@
+var 	express 	= require('express'),
+	swig 		= require('swig'),
+	server 	= express(),
+	bodyParser 	= require('body-parser'),
+	mysql		= require('mysql'),
+	nodemailer 	= require('nodemailer');
+
+server.use( bodyParser() )
+server.engine('html', swig.renderFile)
+server.set('view engine', 'html')
+server.set('views', './app/views')
+swig.setDefaults({
+	cache : false
+});
+
+server.use(express.static(__dirname + '/public'))
+
+var conn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'congreso'
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'pastoraljuvenilmonteria15@gmail.com',
+        pass: 'pastoral2015'
+    }
+});
+
+var sendmail = function(mail, nombre){
+	var mailOptions = {
+	    from: 'pastoraljuvenilmonteria15@gmail.com', // sender address
+	    to: mail, // list of receivers
+	    subject: 'Confirmacion de Registro', // Subject line
+	    text: 'Felicidades usted se registro con exito!!!',// plaintext body
+	    html: '<h1 style="color: blue; text-align: center; font-size: 3em">Registro Exitoso</h1>\n<p style="text-align: center; font-size: 2em; text-align:justify"> coordial saludo '+nombre+', a buena hora has quedado registrado  en nuestra base de datos para asistir al <strong>XlX congreso Diocesano de Jovenes.</strong> cualquier inquietud puedes escribir a este correo.</p>' // html body
+	}
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        return console.log(error);
+	    }
+	    console.log('Message sent: ' + info.response);
+
+	})
+}
+
+server.get('/form', function (req, res){
+	res.render('formRegister')
+})
+
+server.post('/add', function (req, res){
+	var object = req.body
+	conn.query( "insert into registrados set ?", object, function (error, results){
+		//debugger
+		if (error){
+			//throw error;
+			if (error.code == "ER_DUP_ENTRY")
+			{
+				res.render('message',{messageError: 'ERROR: ya existe el usuario '+ req.body.Correo})
+			} else {
+				res.render('message',{messageError:'ERROR intentalo de Nuevo'})
+			}
+			//res.render('message', { user_session: req.session.username, mensajeError: 'ERROR: '+error, mensaje: '' })
+		} else {
+			sendmail(object.Correo, object.Nombres)
+			res.render('message', {message: 'Registro Correcto!', alert:"Felicidades!, se ha enviado un correo a la direccion " + object.Correo})
+		}//res.render('message',{  user_session: req.session.username, mensaje: 'El Usuario: '+object.var_4+' '+object.var_6+' se agrego Correctamente!!', mensajeError: '' });
+	})
+	//debugger
+})
+
+server.get('/Registrados', function (req, res){
+	conn.query('select * from registrados', function (error, rows, fields){
+		res.render('users', {array: rows})
+	})
+})
+
+server.use(function(req, res){
+	res.status(404).send('notfound')
+})
+
+
+ server.listen(4000, function(){console.log('server runnig on port 3000')})
